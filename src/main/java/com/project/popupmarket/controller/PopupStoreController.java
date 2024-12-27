ccf617ac00a1c3d9afbc1c42a8e1c340b28636d2
yeonsinkeem <dudtls0522@sookmyung.ac.kr>
2024-12-28 00:00:12 +0900

    feat: implement Create and Read
    - PopupStore 생성 기능 추가
    - 개별 PopupStore 조회
    - 필터링된 PopupStore 조회

--- src/main/java/com/project/popupmarket/controller/PopupStoreController.java
+++ src/main/java/com/project/popupmarket/controller/PopupStoreController.java
@@ -1,18 +1,25 @@
 package com.project.popupmarket.controller;
 
 
+import com.project.popupmarket.dto.PopupStoreImgDTO;
 import com.project.popupmarket.dto.PopupStoreTO;
+import com.project.popupmarket.repository.PopupStoreImageJpaRepository;
 import com.project.popupmarket.repository.PopupStoreJpaRepository;
+import com.project.popupmarket.service.PopupStoreFileStorageService;
 import com.project.popupmarket.service.PopupStoreServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,36 +29,63 @@ public class PopupStoreController {
     private PopupStoreJpaRepository popupStoreJpaRepository;
     @Autowired
     private PopupStoreServiceImpl popupStoreServiceImpl;
+    @Autowired
+    private PopupStoreFileStorageService popupStoreFileStorageService;
+    @Autowired
+    private PopupStoreImageJpaRepository popupStoreImageJpaRepository;
 
-    // 1. Create
-    @PostMapping("/mypage/popup/add")
+    // [ CREATE ]
+    @PostMapping(value = "/mypage/popup/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "팝업스토어 추가")
-    public ResponseEntity<String> createPopup(@RequestBody PopupStoreTO popupStore) {
-        PopupStoreTO to = new PopupStoreTO();
+    public ResponseEntity<String> createPopup(
+            @RequestPart("popupStore") PopupStoreTO popupStore, @RequestPart("thumbnail") MultipartFile thimg, @RequestPart("images") List<MultipartFile> images) {
+        try {
+            List<String> imgNames = new ArrayList<>();
+            long popupCount = popupStoreJpaRepository.count();
 
-        // long popupSeq = popupStore.getSeq();
-        // to.setPopup_user_seq(popupStore.getPopup_user_seq());
-        to.setThumbnail(popupStore.getThumbnail());
-        to.setType(popupStore.getType());
-        to.setTargetAgeGroup(popupStore.getTargetAgeGroup());
-        to.setTitle(popupStore.getTitle());
-        to.setDescription(popupStore.getDescription());
-        to.setTargetLocation(popupStore.getTargetLocation());
-        to.setStartDate(popupStore.getStartDate());
-        to.setEndDate(popupStore.getEndDate());
+            // 1. 썸네일 이미지 저장
+            String thPath = "src/main/resources/static/images/popup_thumbnail";
 
-        int flag = popupStoreServiceImpl.insert(to);
+            // 썸네일 파일 이름 지정
+            String originalFilename = thimg.getOriginalFilename();
+            String thPath2 = "images/popup_thumbnail/";
+            String ext = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+            String thumbNailFileName = "popup_" + (popupCount + 1) + "_thumbnail." + ext;
 
-        if (flag == 1) {
-            return new ResponseEntity<>("팝업스토어가 성공적으로 추가되었습니다.", HttpStatus.CREATED);
-        } else if (flag == 0) {
-            return new ResponseEntity<>("팝업스토어 추가에 실패했습니다.", HttpStatus.BAD_REQUEST);
-        } else {
-            return new ResponseEntity<>("서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+            popupStoreFileStorageService.storeFile(thimg, thPath, thumbNailFileName);
+
+            // 2. 상세 이미지 저장
+            String imagesPath = "src/main/resources/static/images/popup_detail";
+            String imagesPath2 = "images/popup_detail/";
+
+            int i = 1;
+            for (MultipartFile img : images) {
+                // 파일 이름 지정
+                String imgOriginalFilename = img.getOriginalFilename();
+                String ext2 = imgOriginalFilename.substring(imgOriginalFilename.lastIndexOf(".") + 1);
+                String imgFileName = "popup_" + (popupCount + 1) +  "_images_" + i + "." + ext2;
+
+                // 파일 저장
+                popupStoreFileStorageService.storeFile(img, imagesPath, imgFileName);
+                imgNames.add(imgFileName);
+
+                i ++;
+            }
+            // 3. 팝업, 썸네일, 상세 이미지 DB 삽입
+            int flag = popupStoreServiceImpl.insert(popupStore, thPath2 + thumbNailFileName, imgNames);
+
+            if (flag == 0) {
+                return new ResponseEntity<>("팝업스토어가 성공적으로 추가되었습니다.", HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>("팝업스토어 추가에 실패했습니다.", HttpStatus.BAD_REQUEST);
+            }
+        } catch (IOException e) {
+            return new ResponseEntity<>("파일 저장 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    // 2 - 1. Read : 조건에 해당하는 팝업들 미리보기 - targetLocation, type, targetAgeGroup, startDate ~ endDate
+    // [ READ ] - 1
+    // : 조건에 해당하는 팝업들 미리보기 - targetLocation, type, targetAgeGroup, startDate ~ endDate
     @GetMapping("/popup/list")
     @Operation(summary = "조건에 해당하는 팝업 리스트")
     public List<PopupStoreTO> getPopupByFilter(
@@ -63,25 +97,28 @@ public class PopupStoreController {
         return popupStoreServiceImpl.findByFilter(targetLocation, type, targetAgeGroup, startDate, endDate);
     }
 
-    // 2 - 2. Read : 번호에 해당하는 팝업
+    // [ READ ] - 2
     // 특정 번호에 해당하는 팝업스토어 상세 정보
     @GetMapping("/popup/detail/{seq}")
     @Operation(summary = "개별 팝업 조회" )
-    public PopupStoreTO getPopupBySeq(@PathVariable Long seq) {
+    public PopupStoreImgDTO getPopupBySeq(@PathVariable Long seq) {
         PopupStoreTO to = popupStoreServiceImpl.findBySeq(seq);
+        List<String> imgLst = popupStoreImageJpaRepository.findById_PopupStoreSeq(seq);
 
-        if ( to == null ) {
+        if (to == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "PopupStore not found with seq: " + seq);
         }
-
-        return to;
+        // return new PopupStoreImgDTO(to, imgLst);
+        return new PopupStoreImgDTO(to, imgLst);
     }
 
     // 2 - 3. Read : 관리 중인 팝업 페이지 목록
     // thumbnail, seq(팝업 기획자의 팝업 데이터), type, title, 입점 요청 몇 회 받았는지
     // @GetMapping("/mypage/popup")
 
+
     // 3. Update : 번호에 해당하는 팝업 수정
+    /*
     @PutMapping("/mypage/popup/edit/{seq}")
     @Operation(summary = "개별 팝업 수정")
     public ResponseEntity<String> updatePopup(@PathVariable Long seq, @RequestBody PopupStoreTO popupStore) {
@@ -93,6 +130,37 @@ public class PopupStoreController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("PopupStore not found");
         }
     }
+     */
+
+
+    // 3. Update : 번호에 해당하는 팝업 수정
+    /*
+    @PutMapping("/mypage/popup/edit/{seq}")
+    @Operation(summary = "개별 팝업 수정")
+    public ResponseEntity<String> updatePopup(@PathVariable Long seq, @RequestBody PopupStoreImgDTO popupStoreImgDTO) {
+        PopupStoreTO to = popupStoreImgDTO.getPopupStore();
+        List<PopupStoreImageList> images = popupStoreImgDTO.getImages();
+
+        // 이때 to는 popupStore 엔티티로 변환된 상황
+        // int result = popupStoreServiceImpl.update(seq, to);
+        UpdatePopupStoreTO resultTO = popupStoreServiceImpl.update2(seq, to, images);
+
+        if (resultTO.getUpdateStatus() > 0) {
+
+     */
+            /*
+            Map<String, Object> responseBody = new HashMap<>();
+            responseBody.put("message", "PopupStore updated succesfully.");
+            responseBody.put("updatedImages", resultTO.getUpdatedImages());
+
+            return ResponseEntity.ok("PopupStore updated successfully!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("PopupStore not found");
+        }
+    }
+
+             */
 
     // 4. Delete
     // 팝업 관리 페이지 "/mypage/popup/view?번호={팝업번호}" -> 해당 팝업에 대한 상세 정보와 삭제하기 버튼 같이 출력
---
+++ src/main/java/com/project/popupmarket/dto/PopupStoreImgDTO.java
@@ -0,0 +1,19 @@
+package com.project.popupmarket.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class PopupStoreImgDTO {
+    private PopupStoreTO popupStore;
+    private List<String> images;
+
+    public PopupStoreImgDTO(PopupStoreTO popupStore, List<String> images) {
+        this.popupStore = popupStore;
+        this.images = images;
+    }
+}
